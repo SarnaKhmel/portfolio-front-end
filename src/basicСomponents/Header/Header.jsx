97ecb63ff0bdf304b449911df2f56630ed517102
sarnakhmel <OlSa@olsa.local>
2023-09-11 18:30:03 +0300

    add locales

--- src/basicСomponents/Header/Header.jsx
+++ src/basicСomponents/Header/Header.jsx
@@ -2,16 +2,48 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+import { useTranslation, initReactI18next } from "react-i18next";
+import i18n from "i18next";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: require("../../Languages/en.json"),
+    },
+    uk: {
+      translation: require("../../Languages/uk.json"),
+    },
+  },
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 const Header = () => {
+  const { t } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
   return (
     <HeaderBlock>
-      <StyledLink to="/"> Home </StyledLink>
-      <StyledLink to="/"> Projects </StyledLink>
-      <StyledLink to="/"> Resume </StyledLink>
-      <StyledLink to="/"> Contacts </StyledLink>
+      <StyledLink to="/">{t("OleksaSarnatskyi")} </StyledLink>
+      <LinkBlock>
+        <StyledLink to="/">{t("Home")}</StyledLink>
+        <StyledLink to="/">{t("Projects")} </StyledLink>
+        <StyledLink to="/">{t("Resume")} </StyledLink>
+        <StyledLink to="/">{t("Contacts")} </StyledLink>
+        <select onChange={(e) => changeLanguage(e.target.value)}>
+          <option value="en">🇺🇸</option>
+          <option value="uk">🇺🇦</option>
+        </select>
+      </LinkBlock>
     </HeaderBlock>
   );
 };
+
+const LinkBlock = styled.div``;
 
 const HeaderBlock = styled.div`
   height: 125px;
--- src/components/MainScreen/MainScreen.jsx
+++ src/components/MainScreen/MainScreen.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import Screen1 from "../Screens/Screen1";
+import { useTranslation, initReactI18next } from "react-i18next";
+import i18n from "i18next";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: require("../../Languages/en.json"),
+    },
+    uk: {
+      translation: require("../../Languages/uk.json"),
+    },
+  },
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 const MainScreen = () => {
+  const { t } = useTranslation();
+
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
   return (
     <>
       <Screen1 />
+      <div>
+        <div>
+          <h3>{t("welcome")}</h3>
+          <p>{t("about")}</p>
+          <p>{t("contact")}</p>
+        </div>
+      </div>
     </>
   );
 };
